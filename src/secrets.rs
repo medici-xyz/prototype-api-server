cac15a4af37fe593c9b3bf49b45cf6161ef8006c
Sayak Chatterjee <Sayak.Chatterjee@tufts.edu>
2022-02-28 22:47:11 -0500

    changing query slightly

--- src/secrets.rs
+++ src/secrets.rs
@@ -1,15 +1,17 @@
 pub const url: &str = "http://localhost:8000/subgraphs/name/wighawag/eip721-subgraph";
 
-pub const query: &str = r#"
-{
-    "query": "{
-        tokenContract(orderBy: numOwners, orderDirection: desc, first: 100) {
-            id
-            name
-            numTokens
-            numOwners
-            supportsEIP721Metadata
-        }
-    }"
-}
-"#;
+// pub const query: &str = r#"
+// {
+//     "query": "{
+//         tokenContract(orderBy: numOwners, orderDirection: desc, first: 100) {
+//             id
+//             name
+//             numTokens
+//             numOwners
+//             supportsEIP721Metadata
+//         }
+//     }"
+// }
+// "#;
+
+pub const query: &str = r#""query": "{\n  tokenContracts(orderBy: numOwners, orderDirection: desc, first: 100) {\n    id\n    name\n    numTokens\n    numOwners\n    supportsEIP721Metadata\n  }\n}""#;
